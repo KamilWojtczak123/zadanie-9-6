8791894841a4b6d4a51cd1e98d01368f6d4b12b8
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-02-04 13:56:32 +0100

    Add html and js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+add.addEventListener('click', function() {
+    var element = document.createElement('li');
+    element.innerHTML = 'item';
+    element.getElementsByTagName('li').length;
+    list.appendChild(element);
+});
