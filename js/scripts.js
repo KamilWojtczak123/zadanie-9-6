1b4bb3833e90753059bea692ade6cac1a0292af3
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-02-06 09:48:13 +0100

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
     var element = document.createElement('li');
-    element.innerHTML = 'item';
+    element.innerHTML = 'item' +
     document.getElementsByTagName('li').length;
-    element.appendChild(element[Number]);
+    element.appendChild(element);
 });
